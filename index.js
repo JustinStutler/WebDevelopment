f34a5482324eabb7b6350eb071588c8ebb99e19c
Wavay <justinstutler36@gmail.com>
2023-02-12 12:15:08 -0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("ToggleBtn").addEventListener("click", function () {
-    var textToToggle = document.getElementById("ToggleTxt");
-    if (textToToggle.style.display === "none") {
-        textToToggle.style.display = "block";
-    } else {
-        textToToggle.style.display = "none";
-    }
-});
